fbffbf2cc034efa3ac0f423d727463251536077a
Mateo <mateo.1459@hotmail.com>
2018-12-01 12:49:28 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urldb;
 if (process.env.NODE_ENV === 'dev') {
     urldb = 'mongodb://localhost:27017/cafe';
 } else {
-    urldb = 'mongodb://cafe_user:a123456@ds123454.mlab.com:23454/cafe'
+    urldb = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urldb;
